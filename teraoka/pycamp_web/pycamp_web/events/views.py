29a7c5a72530e486540302ddc8c031c6480eb01e
hiroshi teraoka <hiroshi4000@gmail.com>
2017-06-04 16:53:16 +0900

    views.pyでページ増やす

--- teraoka/pycamp_web/pycamp_web/events/views.py
+++ teraoka/pycamp_web/pycamp_web/events/views.py
@@ -5,6 +5,8 @@ from documents.models import Document
 from django.http import HttpResponse   # httpのレスポンスを返す
 from django.http import Http404
 
+from django.core.paginator import Paginator  # ページネーションを作成する
+
 
 class EventListView(View):
 
@@ -14,11 +16,17 @@ class EventListView(View):
         # events = models.Event.objects.all()  # Eventモデルのインスタンス objects.all()は全て取得 Event.documents.all でイベントのドキュメントを全部取得できる
         events = models.Event.objects.all()
         # raise Exception("event", [i for i in documents])
-
+        p = Paginator(events, 20)
+        p_page = request.GET.get("p", 1)
+        page = p.page(p_page)
+        events = page.object_list
+        # raise Exception(events.page_range(1).object_list)
         #return HttpResponse((['%s %s %s %s<br>' % (event.title, event.detail, event.start, event.end) for event in events]))   # OKを返すビューを作った
-        return render(request, 'events/events_list.html', {'events': events})
+        return render(request, 'events/events_list.html', {'events': events})  # {'events': events}
 
-
+# def get(request) このrequestの中にはたくさんの情報が入ってきている
+#   urlの (127.0.0.1:8000/events/?p=3)など　get情報を取得する場合も使えます
+#   page = request.GET.get("p", 1)  GETパラメーターを取得します、p の値　なければデフォルトは１ですよ
 
 class EventDetailView(View):  # イベントの詳細ページを取得
     def get(self, request, event_id):
