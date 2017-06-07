739e1ed35dd198958abbb3e4350a1b382d665876
hiroshi teraoka <hiroshi4000@gmail.com>
2017-06-08 07:52:28 +0900

    ratingファイル完成6_8

--- teraoka/movielens_corr/movielens_corr/rating/urls.py
+++ teraoka/movielens_corr/movielens_corr/rating/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from rating.views import DataUploadView
+from rating.views import DataUploadView, ScatterPlotView
 
 
 
 urlpatterns = [
     url(r'^$', DataUploadView.as_view(), name='list'),  # as_view()はクラスベースで作った場合は定義しないといけない
-    # url(r'^test/', Test.as_view()),
+    url(r'^scatterplot/', ScatterPlotView.as_view()),
 ]
--- teraoka/movielens_corr/movielens_corr/rating/views.py
+++ teraoka/movielens_corr/movielens_corr/rating/views.py
@@ -6,13 +6,17 @@ from rating.form import UploadFileForm
 from rating.models import Movie, UserProfile, Rating
 import pandas as pd
 from io import BytesIO, StringIO
+from django.db.models import Avg
+from django.http import HttpResponse
 
 """
 BytesIOがfile
 """
 
 class DataUploadView(View):
-
+    """
+    データをサーバーへUploadする
+    """
     def get(self, request):
         return render(request, 'rating/data_upload.html', {})
 
@@ -48,13 +52,48 @@ class DataUploadView(View):
             else:
                 break
 
-        # raise Exception("tt", rating_data)
-        # raise Exception("test", movies['title'], users['gender'])
-
-
-
+        # raise Exception("tt", rating_df)
 
         return render(request, 'rating/upload_complete.html',
                       {"movies_title": movies.title,
                        "users_title": users.gender,
                        "ratings_title": ratings.rating})
+
+class ScatterPlotView(View):
+
+    def get(self, request):
+
+        movies = Movie.objects.all()
+        movies_ratings = {'movie':[], 'male_average':[], 'felale_average':[]}
+        for movie in movies:
+            ratings = Rating.objects.filter(movie__mid=movie.mid)
+            #raise Exception("tt %s" % ratings)
+            male_ratings = ratings.filter(user_profile__gender='M')
+            felale_ratings = ratings.filter(user_profile__gender='F')
+            # male_average = male_ratings / len(male_average)
+            # felale_average = felale_ratings / len(felale_ratings)
+            male_average = male_ratings.aggregate(Avg('score'))
+            felale_average = felale_ratings.aggregate(Avg('score'))
+
+            movies_ratings['movie'].append(movie)
+            movies_ratings['male_average'].append(male_average)
+            movies_ratings['felale_average'].append(felale_average)
+
+        movie_ratings_df = pd.DataFrame.from_dict(movies_ratings)
+        response = HttpResponse(content_type='text/csv')
+        response['Content - Disposition'] = 'attachment;filename ="filename.csv"'
+        movie_ratings_df.to_csv(response)
+        return response
+
+
+
+
+            #return render(request, 'rating/scatterplotview.html', {'movie_ratings_df', movie_ratings_df})
+
+            # raise Exception("エラー ：%s" % gender)
+            #
+            # raise Exception("movies%s" %  movie.mid)
+        #movie = Rating.objects.filter()
+        #raise Exception("movies", movies.mid[0])
+
+        #return render(request, 'rating/scatterplotview.html',{'movies', movies})
