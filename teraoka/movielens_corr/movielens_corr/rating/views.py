fa66b0dfd94a101f1cd29b6eb6e6fecc0338d177
hiroshi teraoka <hiroshi4000@gmail.com>
2017-06-09 09:08:47 +0900

    映画RatingをD3で描画完成

--- teraoka/movielens_corr/movielens_corr/rating/views.py
+++ teraoka/movielens_corr/movielens_corr/rating/views.py
@@ -8,6 +8,7 @@ import pandas as pd
 from io import BytesIO, StringIO  # BytesIOはバイトファイルを返還処理する組込みメソッド
 from django.db.models import Avg
 from django.http import HttpResponse
+from django.db import transaction
 
 
 class DataUploadView(View):
@@ -20,36 +21,54 @@ class DataUploadView(View):
 
     def post(self, request):
 
-        # movies = Movie()
-        # movie_df = pd.read_csv(BytesIO(request.FILES["movie-file"].read()),
-        #                      sep='::', header=None,
-        #                      names=['movie_id', 'title', 'genres'], encoding='ISO-8859-1')
-        # for _, (mid, title, _) in movie_df.iterrows():  # pandasの行を一度にとってくる
-        #     movies = Movie(mid=mid, title=title)     # 使わないものは _ にする
-        #     movies.save()
-        #
-        # users = UserProfile()
-        # user_df = pd.read_csv(BytesIO(request.FILES["user-file"].read()),
-        #                      sep='::', header=None,
-        #                      names=['user_id', 'gender', 'age', 'occupation', 'zip'], encoding='ISO-8859-1')
-        # for _, (user_id, gender, _, _, _) in user_df.iterrows():
-        #     users = UserProfile(uid=user_id, gender=gender)
-        #     users.save()
-        #
-        # ratings = Rating()
-        # rating_df = pd.read_csv(BytesIO(request.FILES["rating-file"].read()),
-        #                      sep='::', header=None,
-        #                      names=['user_id', 'movie_id', 'rating', 'timestamp'], encoding='ISO-8859-1')
-        # count = 0
-        # for _, (uid, mid, rating, _) in rating_df.iterrows():
-        #     count += 1
-        #     if count < 3000:
-        #         user_profile = UserProfile.objects.get(uid=uid)  # UserProfileクラスのuidをとってくる
-        #         movie = Movie.objects.get(mid=mid)  # Movieクラスのmidをとってくる
-        #         ratings = Rating.objects.create(user_profile=user_profile, movie=movie, score=rating)  # 上でとっているidをRatingクラスに入れる(紐付ける)
-        #         ratings.save()
-        #     else:
-        #         break
+        movies = Movie()
+        movie_df = pd.read_csv(BytesIO(request.FILES["movie-file"].read()),
+                             sep='::', header=None,
+                             names=['movie_id', 'title', 'genres'], encoding='ISO-8859-1')
+        with transaction.atomic():
+            for _, (mid, title, _) in movie_df.iterrows():  # pandasの行を一度にとってくる
+                movies = Movie(mid=mid, title=title)     # 使わないものは _ にする
+                movies.save()
+
+        users = UserProfile()
+        user_df = pd.read_csv(BytesIO(request.FILES["user-file"].read()),
+                             sep='::', header=None,
+                             names=['user_id', 'gender', 'age', 'occupation', 'zip'], encoding='ISO-8859-1')
+        with transaction.atomic():
+            for _, (user_id, gender, _, _, _) in user_df.iterrows():
+                users = UserProfile(uid=user_id, gender=gender)
+                users.save()
+
+        ratings = Rating()
+        rating_df = pd.read_csv(BytesIO(request.FILES["rating-file"].read()),
+                             sep='::', header=None,
+                             names=['user_id', 'movie_id', 'rating', 'timestamp'], encoding='ISO-8859-1')
+
+        rating_df_grouped = (rating_df.groupby('movie_id').size())
+        # raise Exception(type(rating_df_grouped))
+        rating_df_grouped = rating_df_grouped.sort_values(ascending=False)
+        # rating_df_grouped = rating_df_grouped.head(20)
+        # raise Exception(rating_df)
+        rating_df = rating_df[rating_df['movie_id'].isin(rating_df_grouped.values)]
+        # raise Exception(rating_df)
+        with transaction.atomic():  # with終わるまで書き込みしない
+            for _, (uid, mid, rating, _) in rating_df.iterrows():
+                # count += 1
+                # if count < 3000:
+                # if rating.count() > 10:
+                user_profile = UserProfile.objects.get(uid=uid)  # UserProfileクラスのuidをとってくる
+                movie = Movie.objects.get(mid=mid)  # Movieクラスのmidをとってくる
+                ratings = Rating.objects.create(user_profile=user_profile, movie=movie,
+                                                score=rating)  # 上でとっているidをRatingクラスに入れる(紐付ける)
+                ratings.save()
+            #raise Exception(rating_df)
+            # \
+            # .sort_values(by=['movie_id'], ascending=False)
+        # movie_ratings_df_top100 = movie_ratings_df.head(100)
+        count = 0
+
+            # else:
+            #     break
 
         return render(request, 'rating/upload_complete.html', {})
 
@@ -76,6 +95,8 @@ class ReadToCsv(View):
             male_ratings = ratings.filter(user_profile__gender='M')
 
             female_ratings = ratings.filter(user_profile__gender='F')
+            # if male_ratings.count() > 10 and female_ratings.count() > 10:
+
             male_average = male_ratings.aggregate(Avg('score'))
             female_average = female_ratings.aggregate(Avg('score'))
 
@@ -86,8 +107,12 @@ class ReadToCsv(View):
         movie_ratings_df = pd.DataFrame.from_dict(movies_ratings)
         movie_ratings_df = movie_ratings_df.fillna(0)
 
+        # movie_ratings_df = (movie_ratings_df.groupby('movie')[['male_average', 'female_average']].sum())\
+        #     .sort_values(by=['male_average', 'female_average'], ascending=False)
+        #
+        # movie_ratings_df_top100 = movie_ratings_df.head(100)
 
-        raise Exception("OK", movie_ratings_df)
+        # raise Exception("OK", movie_ratings_df)
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment;filename ="rating.csv"'
         movie_ratings_df.to_csv(response, index=False)
